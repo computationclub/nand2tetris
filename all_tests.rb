a42dc696a4b22f1c24bbeefed2683c2aa5f0b9f1
James Mead <james@floehopper.org>
2014-12-22 20:25:35 +0000

    Add rake task to run test scripts for chapters 1-3.
    Use `TESTOPTS=-v rake` to obtain verbose output.

---
+++ all_tests.rb
@@ -0,0 +1,14 @@
+require 'minitest/autorun'
+require 'open3'
+
+SCRIPT = File.expand_path(File.join('tools', 'HardwareSimulator.sh'))
+
+class HardwareSimulatorTest < Minitest::Test
+  Dir['projects/0[1-3]/*.tst'].map do |test_file|
+    define_method "test_#{File.basename(test_file, '.tst')}" do
+      command = "#{SCRIPT} #{test_file}"
+      output, status = Open3.capture2e(command)
+      assert status.success?, output
+    end
+  end
+end
