509ba5f800a63f1a2e82b082262463afc67c1c71
James Mead <james@floehopper.org>
2015-01-13 19:44:30 +0000

    Chapter 4 multiplication passing test.

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -6,4 +6,26 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+// set R2 to 0
+@R2
+M=0
+(LOOP)
+// jump to "end" if R0 is zero
+@R0
+D=M
+@END
+D;JEQ
+// add R1 to R2
+@R1
+D=M
+@R2
+M=D+M
+// decrement R0
+@R0
+M=M-1
+// jump to "loop"
+@LOOP
+0;JMP
+(END)
+@END
+0;JMP
