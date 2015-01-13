df291edf74340c97ba50b33ee064a877e4d26047
James Mead <james@floehopper.org>
2015-01-13 20:47:13 +0000

    Our first attempt at the Fill exercise in chapter 4.

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -8,4 +8,40 @@
 // i.e. writes "black" in every pixel. When no key is pressed, the
 // program clears the screen, i.e. writes "white" in every pixel.
 
-// Put your code here.
+(BEGINNING)
+  // Set screen pointer to address at start of screen
+  @SCREEN
+  D=A
+  @R0
+  M=D
+(HALFWAY)
+// Read the current value of the keyboard (@KBD)
+  @KBD
+  D=M
+// Paint colour based on keyboard (0=white; 1=black)
+  @WHITE
+  D;JEQ
+  @R0
+  A=M
+  M=-1
+  @FURTHERDOWN
+  0;JMP
+(WHITE)
+  @R0
+  A=M
+  M=0
+// Increment screen pointer
+(FURTHERDOWN)
+  @R0
+  M=M+1
+// If screen pointer is more than limit jump to BEGINNING
+  D=M
+  @8192
+  D=D-A
+  @SCREEN
+  D=D-A
+  @BEGINNING
+  D;JGT
+// Else jump to HALFWAY
+  @HALFWAY
+  0;JMP
